f88fa09161d884490c79321bd8c2b4c5b3ec9982
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-12 15:17:14 +0300

    feat: implement getExamGrade method

--- src/main/java/exo/Teacher.java
+++ src/main/java/exo/Teacher.java
@@ -1,20 +1,30 @@
 package exo;
 
+import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Teacher extends Person{
-    private Speciality speciality;
+public class Teacher extends Person {
+  private Speciality speciality;
 
-    public double getExamGrade(Exam exam, Student student, Instant t){
-        return 0.0;
-    }
+  public double getExamGrade(Exam exam, Student student, Instant t) throws Exception {
+    return student.getGrades().stream()
+        .filter(grade -> grade.getExam().equals(exam))
+        .findFirst()
+        .map(
+            grade -> {
+              try {
+                return grade.getGradeByInstant(t);
+              } catch (Exception e) {
+                throw new RuntimeException(e);
+              }
+            })
+        .orElseThrow(() -> new Exception("No grade found for exam " + exam + " at instant " + t));
+  }
 }
