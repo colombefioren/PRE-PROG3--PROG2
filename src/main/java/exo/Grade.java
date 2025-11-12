127b64067bfe51d6b9ca9a4faf50d65027b92ad4
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-12 15:09:52 +0300

    feat: getGradeByInstant method in grade model

--- src/main/java/exo/Grade.java
+++ src/main/java/exo/Grade.java
@@ -1,19 +1,25 @@
 package exo;
 
+import java.time.Instant;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Grade {
-    private double initialValue;
-    private List<GradeHistory> gradeHistoryList;
-    private Exam exam;
-    private Student student;
+  private double initialValue;
+  private List<GradeHistory> gradeHistoryList;
+  private Exam exam;
+  private Student student;
 
-
+  public double getGradeByInstant(Instant instant) throws Exception {
+    return gradeHistoryList.stream()
+        .filter(gradeHistory -> gradeHistory.getDateOfModification().equals(instant))
+        .findFirst()
+        .map(GradeHistory::getGrade)
+        .orElseThrow(() -> new Exception("No grade found for instant: " + instant));
+  }
 }
