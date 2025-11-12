3b1471afe83215ad07c23a4b456d15180a15abc8
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-12 14:59:10 +0300

    fix: make grade history a list

--- src/main/java/exo/Grade.java
+++ src/main/java/exo/Grade.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Grade {
     private double initialValue;
-    private GradeHistory gradeHistory;
+    private List<GradeHistory> gradeHistoryList;
     private Exam exam;
     private Student student;
 }
