78c0f1f24c4d520911a872f4434aaa9b18450607
colombefioren <colomberakotonjanahary@gmail.com>
2025-11-12 14:59:56 +0300

    refactor: change date of birth type to Date instead of Instant

--- src/main/java/exo/Person.java
+++ src/main/java/exo/Person.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -13,7 +13,7 @@ public class Person {
     private int id;
     private String firstName;
     private String lastName;
-    private Instant dateOfBirth;
+    private Date dateOfBirth;
     private String email;
     private String phoneNumber;
 }
